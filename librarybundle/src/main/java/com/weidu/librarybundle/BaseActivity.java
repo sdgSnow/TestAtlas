6770494cbbad5e25d0450296663f8a445404a594
sdgSnow <2439463298@qq.com>
2018-07-21 09:47:15 +0800

    atlas接入提交各个module

---
+++ librarybundle/src/main/java/com/weidu/librarybundle/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.weidu.librarybundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+    }
+}
